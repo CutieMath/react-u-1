cea75d7198d5f248ab63cfa0c5038ea511d7e2df
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-16 14:27:01 +1000

    use implicit return properly

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import Card from "../card-container/card-container.component";
 import "./card-list.styles.css";
 
-const CardList = ({ monsters }) => {
-  return (
-    <div className="card-list">
-      {monsters.map((monster, index) => {
-        return <Card monster={monster} key={index} />;
-      })}
-    </div>
-  );
-};
+const CardList = ({ monsters }) => (
+  // implicit return
+  <div className="card-list">
+    {monsters.map((monster, index) => {
+      return <Card monster={monster} key={index} />;
+    })}
+  </div>
+);
 
 export default CardList;
