61eb18b99b172fda8c512989ceb235dc19ab960e
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-16 14:29:08 +1000

    use implicit return in all needed components

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "./search-box.styles.css";
 
-const SearchBox = ({ placeholder, onChangeHandler, className }) => {
-  return (
-    <input
-      type="search"
-      className={`search-box ${className}`} // search-box here is a generalised style for the component
-      placeholder={placeholder}
-      onChange={onChangeHandler}
-    />
-  );
-};
+const SearchBox = ({ placeholder, onChangeHandler, className }) => (
+  <input
+    type="search"
+    className={`search-box ${className}`} // search-box here is a generalised style for the component
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
 
 export default SearchBox;
